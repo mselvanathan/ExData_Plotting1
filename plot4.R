d206b00021e9270f94c3be5f7292fe226485ae71
mselvanathan <mselvanathan@yahoo.com>
2014-05-13 17:27:00 -0400

    Revised plot 4
    Plot 4 redone after data adequately subsetted

--- plot4.R
+++ plot4.R
@@ -15,18 +15,18 @@ png("plot4.png", width = 480, height = 480)
 par(mfrow=c(2,2), mar=c(4,4,2,1))
 
 #Graph1.1
-hist(as.numeric(afinal$Global_active_power), col="red", main = "Global Active Power", xlab="Global Active Power (kilowatts)", axes = TRUE, plot = TRUE, labels = FALSE)
+plot(as.numeric(afinal$Global_active_power), type = "l", ylab = "Global Active Power", xlab ="")
 
 #Graph1.2
 plot(afinal$datetime, afinal$Voltage, type="l", xlab="datetime", ylab="Voltage")
 
 #Graph2.1
-plot(as.numeric(afinal$Sub_metering_1)~afinal$datetime, type="l", xlab="", ylab="Energy sub metering", ylim=c(0,35))
-legend("topright",legend =c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1), col=c("black", "red", "blue"),cex = 0.7, lwd=1, y.intersp=.45)
+plot(as.numeric(afinal$Sub_metering_1)~afinal$datetime, type="l", ylab = "Energy sub metering", xlab ="")
 lines(as.numeric(afinal$Sub_metering_2)~afinal$datetime,type="l", col="red")
 lines (afinal$Sub_metering_3~afinal$datetime,type="l", col="blue")
+legend("topright",legend =c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1), col=c("black", "red", "blue"),cex = 0.7, lwd=1, y.intersp=.65)
 
 #Graph2.2
-plot(afinal$datetime, afinal$Global_active_power, type="l", xlab="", ylab="Global Active Power(kilowatts)")
+plot(afinal$datetime, afinal$Global_reactive_power, type="l", xlab="", ylab="Global_reactive_power")
 dev.off()
 
